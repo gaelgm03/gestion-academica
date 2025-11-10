db524ead3ce93251838a4650c05ee09fd48ae0b4
Gael Guzmán <gguzmun@gmail.com>
2025-11-10 09:28:53 -0600

    Integrar carpeta frontend (Angular) al repositorio principal

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Docentes } from './docentes/docentes';
+import { Incidencias } from './incidencias/incidencias';
+import { Dashboard } from './dashboard/dashboard';
+
+const routes: Routes = [
+  { path: 'docentes', component: Docentes },
+  { path: 'incidencias', component: Incidencias },
+  { path: 'dashboard', component: Dashboard },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/incidencias/incidencias.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Incidencias } from './incidencias';
+
+describe('Incidencias', () => {
+  let component: Incidencias;
+  let fixture: ComponentFixture<Incidencias>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Incidencias]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Incidencias);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
